123cf2bcff01b026b84ec64cbdef0a6888f760e1
matt-standley <Matthew@matts-MacBook-Pro-2.local>
2017-10-04 16:32:17 -0700

    test xcode project

---
+++ Book Exchange/Book Exchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Book Exchange
+//
+//  Created by matt standley on 10/4/17.
+//  Copyright © 2017 SoftwareEngineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
